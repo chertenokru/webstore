e657c19b1f40e69b54e42e7daad9fb3f0fbb6d17
Vladimir <chertenokru@users.noreply.github.com>
2018-08-03 10:25:30 +0300

    Update SettingFilter.java

--- src/main/java/ru/chertenok/webapps/webstore/filter/SettingFilter.java
+++ src/main/java/ru/chertenok/webapps/webstore/filter/SettingFilter.java
@@ -17,6 +17,7 @@ public class SettingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
         //servletResponse.setContentType("text/html; charset=UTF-8");
         //  System.out.println(servletResponse.getCharacterEncoding());
         filterChain.doFilter(servletRequest, servletResponse);
