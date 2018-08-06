aec0eb44ca0b3a02037a47c8ed737e5783bc91fd
chertenokru <admin@chertenok.ru>
2018-08-06 18:35:36 +0300

    model update

--- src/main/java/ru/chertenok/webapps/webstore/model/Item.java
+++ src/main/java/ru/chertenok/webapps/webstore/model/Item.java
@@ -1,7 +1,25 @@
 package ru.chertenok.webapps.webstore.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
+    @Id
+    String no;
+    Brand brand;
+    String description;
+    String country;
+    String sex;
+    Catalog catalog;
+    CollectionItem collectionItem;
+    String article;
+    String compaund;
+    String compaundFull;
+    Framework framework;
+
+
+
+
+
 }
